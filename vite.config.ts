d4fb8ad6f61c1dbf497cf5afd73b015dc607bc1e
cyclingbyte <cyclingbyte@web.de>
2024-08-27 00:17:08 +0200

    formatter

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,30 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'node:url'
-import dts from 'vite-plugin-dts'
+import { fileURLToPath, URL } from 'node:url';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [dts()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   build: {
     lib: {
-      entry: fileURLToPath(new URL('./src/leaflet-sidepanel.ts', import.meta.url)),
+      entry: fileURLToPath(
+        new URL('./src/leaflet-sidepanel.ts', import.meta.url)
+      ),
       formats: ['es', 'cjs', 'umd'],
       name: 'leaflet-sidepanel',
-      fileName: (format) => `leaflet-sidepanel.${format}.js`
+      fileName: (format) => `leaflet-sidepanel.${format}.js`,
     },
     rollupOptions: {
       external: ['leaflet'],
       output: {
         globals: {
           leaflet: 'L',
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
