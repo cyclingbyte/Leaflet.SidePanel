2019829a95c5e537c3a91147b42b86297bb191ba
cyclingbyte <cyclingbyte@web.de>
2024-08-27 18:08:57 +0200

    chore: migrate to typescript (#16)
    * chore: migrate to typescript
    * chore: change prettier workspace

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [dts()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(
+        new URL('./src/leaflet-sidepanel.ts', import.meta.url)
+      ),
+      formats: ['es', 'cjs', 'umd'],
+      name: 'leaflet-sidepanel',
+      fileName: (format) => `leaflet-sidepanel.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['leaflet'],
+      output: {
+        globals: {
+          leaflet: 'L',
+        },
+      },
+    },
+  },
+});
